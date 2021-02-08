8a731af18dc98b79058a7dc5120f05ef5c7a27f5
Panos Hapsides <p.hapsides@live.com>
2021-02-08 16:39:39 +0200

    Remove detail component reference

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,7 +1,7 @@
 
 import './App.scss';
 import Recipe from './Recipe/Recipe';
-import Detail from './Detail/Detail';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 function App() {
@@ -10,7 +10,6 @@ function App() {
 			<article className="App">
 				<Switch>
 					<Route exact path="/" component={Recipe} />
-					<Route exact path="/:apiRef" component={Detail} />
 				</Switch>
 			</article >
 		</Router>
