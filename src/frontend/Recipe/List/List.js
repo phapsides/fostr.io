791fdfd7558cde08a66df74b7870044ef6cd6ac3
Panos Hapsides <p.hapsides@live.com>
2021-02-09 10:47:03 +0200

    Apply Material card

--- src/frontend/Recipe/List/List.js
+++ src/frontend/Recipe/List/List.js
@@ -1,25 +1,98 @@
 import React from 'react';
 import './List.scss';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
 
 const List = (props) => {
 
     const { recipes } = props;
+    const classes = useStyles();
+ 
 
     if (!recipes || recipes.length === 0) return <p>No recipes, sorry</p>;
 
     return (
         <>
-            <ul className="recipe-list">
+            <div className="recipe-list">
                 {recipes.map((recipe) => {
                     return (
-                        <Link to={`/${recipe.title}`} className="recipe-title">
-                            <li key={recipe.apiRef}>{recipe.title}</li>
+                        <Link
+                            to={`/${recipe.title}`}
+                            className="recipe-title"
+                        >
+
+
+                            <Card className={classes.root}>
+                                <CardHeader
+                                    title={recipe.title}
+                                />
+                                <CardMedia
+                                    className={classes.media}
+                                    image="/static/images/cards/paella.jpg"
+                                    title="Paella dish"
+                                />
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {recipe.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions disableSpacing>
+                                    <IconButton aria-label="add to favorites">
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="share">
+                                        <ShareIcon />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
                         </Link>
                     );
                 })}
-            </ul>
+            </div>
         </>
     );
 };
 export default List;
+
+
+
